6a818f7d98737376fe20b7ce5ba31134672d64b1
TJ <thaddeusjiang@gmail.com>
2021-06-06 23:18:35 +0900

    update

--- src/SignInSection.tsx
+++ src/SignInSection.tsx
@@ -9,7 +9,7 @@ const uiConfig = {
   // We will display Google and Facebook as auth providers.
   signInOptions: [
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
   ],
   callbacks: {
     // Avoid redirects after sign-in.
